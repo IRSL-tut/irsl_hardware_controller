f8d9e9b937849cdf885f3b5103a37c1f61ed39f5
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2025-11-26 18:23:30 +0900

    add opt_parse.hpp

--- irsl_common_utils/include/irsl/opt_parse.hpp
+++ irsl_common_utils/include/irsl/opt_parse.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "irsl/thirdparty/CLI11.hpp"
+#include <stdlib.h>
+
+namespace irsl_shm_controller
+{
 
 class OptParse : public CLI::App
 {
@@ -8,12 +12,21 @@ private:
     uint64_t _hash;
     uint32_t _key;
 public:
-    OptParse(const std::string name) : CLI::App(name)
+    OptParse(const std::string &name) : CLI::App(name)
     {
         add_option("--hash", _hash, "")->default_val("8888");
         add_option("--shm_key", _key, "")->default_val("8888");
-        allow_extras(true);
     }
     uint64_t getHash() { return _hash; }
     uint32_t getShmKey() { return _key; }
+    void opt_parse(int argc, char **argv)
+    {
+        try {
+            this->parse(argc, argv);
+        } catch(const CLI::ParseError &e) {
+            ::exit( this->exit(e) );
+        }
+    }
 };
+
+}
